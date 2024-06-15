ccea854d8d01a537b3d5cf38569f7e14844f6d55
Nikhil Verma <vermanikhil1025@gmail.com>
2024-06-15 17:47:55 +0000

    datatype

---
+++ .devcontainer/01_basics/Variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "nikhil@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2  //not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengluru"
+
+console.log(accountId);
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+
+Use let and const to declare a varibale. Don't use var due to block scope & functional scope.
+
+*/
